e88cff5cfe6deef86c57cb11aa70251abfa0fae1
Ohana Luz <123988706+ohanaluz@users.noreply.github.com>
2024-03-22 11:21:07 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
-const express = require('express');
-const {engine} = require('express-handlebars');
+const express = require("express")
+const app = express()
+const handlebars = require("express-handlebars").engine
+const bodyParser = require("body-parser")
+const post = require("./models/post")
 
-const app = express();
+app.engine("handlebars", handlebars({defaultLayout: "main"}))
+app.set("view engine", "handlebars")
 
-app.engine('.handlebars', engine({
-    defaultLayout: 'layout',
-    layoutDir: __dirname + '/views/layouts'
-}));
-app.set ('view engine', 'handlebars');
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
-app.get ('/', (req, res)=>{
-    res.render('index');
-});
+app.get("/", function(req, res){
+    res.render("index")
+})
 
-app.get ('/consultar', (req, res)=>{
-    res.render('consultar');
-});
+app.post("/atualizar", function(req, res){
+    post.create({
+        nome: req.body.nome,
+        telefone: req.body.telefone,
+        origem: req.body.origem,
+        data_contato: req.body.data_contato,
+        observacao: req.body.observacao
+    }).then(function(){
+        res.redirect("/")
+    }).catch(function(erro){
+        res.send("Falha ao cadastrar os dados: " + erro)
+    })
+})
 
-app.get ('/atualizar', (req, res)=>{
-    res.render('atualizar');
-});
+app.get("/atualizar", function(req, res){
+    res.render("atualizar")
+})
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log ('Servidor rodando')
+app.listen(8081, function(){
+    console.log("Servidor Ativo!")
 })
