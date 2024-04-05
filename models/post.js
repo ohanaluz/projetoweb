679626ffaada67ed13483e8107e33793b4da31de
Ohana Luz <123988706+ohanaluz@users.noreply.github.com>
2024-04-05 11:25:15 -0300

    Add files via upload

--- models/post.js
+++ models/post.js
@@ -1,23 +1,10 @@
 const db = require("./banco")
-
-const Agendamentos = db.sequelize.define("agendamentos",{
-    nome:{
-        type: db.Sequelize.STRING
-    },
-    telefone:{
-        type: db.Sequelize.INTEGER
-    },
-    origem:{
-        type: db.Sequelize.STRING
-    },
-    data_contato:{
-        type: db.Sequelize.DATE
-    },
-    observacao:{
-        type: db.Sequelize.TEXT
-    }
-})
-
-//Agendamentos.sync({force: true})
-
-module.exports = Agendamentos
+const Agendamentos = db.sequelize.define("agendamentos",
+{    nome:{        type: db.Sequelize.STRING    },   
+    telefone:{        type: db.Sequelize.INTEGER    },    
+    origem:{        type: db.Sequelize.STRING    },    
+    data_contato:{        type: db.Sequelize.DATE    },    
+    observacao:{        type: db.Sequelize.TEXT    }})
+    
+    Agendamentos.sync({force: true})
+    module.exports = Agendamentos
